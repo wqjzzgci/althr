babd5979fdcc96f88ba8833eabc5cb2f5c3a17de
wqjzzgci <hayaqi0504@gmail.com>
2017-07-03 11:44:49 +0800

    hello

--- FFFT.cpp
+++ FFFT.cpp
@@ -1,9 +1,31 @@
 #include<stdio.h>
-#include<iostream>
-int main(int argc,char *args[]){
-    int j,k;
-    printf("hello world");
-    std::cout<<"今天怎么了";
+/*
+题目1：有1,2,3,4个数字，能组成多少互不相同且无重复数字的三位数？都是多少？
+*/
+int main(int argc,char **argv)
+{
+    int i,j,k;
+    printf("\n");
+    int num=0;
+    for(i=1;i<5;i++)
+    {
+        for(j=1;j<5;j++)
+        {
+            for(k=1;k<5;k++)
+            {
+                if(i!=j&&i!=k&&j!=k)
+                {
+                    printf("%d,%d,%d",i,j,k);
+                    printf("\n");
+                    printf("%d",i*100+j*10+k);
+                    printf("\n");
+                    num++;
+                }
+            }
+        }
+    }
+    printf("\n");
+    printf("%d",num);
+    getchar();
     return 0;
 }
-
